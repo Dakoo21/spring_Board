8b6caf661afec7d50e5181c22af4c4c19b2b8cce
JJH <nwan0947@gmail.com>
2024-07-04 11:03:29 +0900

    first commit

---
+++ src/main/java/com/myboard/board/service/BoardService.java
@@ -0,0 +1,16 @@
+package com.myboard.board.service;
+
+import com.myboard.board.dto.BoardDTO;
+import com.myboard.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+    public void save(BoardDTO boardDTO) {
+        boardRepository.save(boardDTO);
+    }
+}
